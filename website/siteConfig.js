1387497192ce5fd55118592e25d2c9aaa5aaa889
Mounika Maddhula <maddhulamounikareddy@gmail.com>
2020-10-02 12:16:01 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
+    title: 'Mounika Reddy Maddhula', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://mounikareddymaddhula.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -26,7 +26,7 @@ const siteConfig = {
     organizationName: 'joatmon08',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-    //   organizationName: 'JoelMarcey'
+    //   organizationName: 'MounikaMaddhula'
 
     // For no header links in the top nav bar -> headerLinks: [],
     headerLinks: [
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'mounikareddymaddhula',
         twitter: '',
-        github: '',
+        github: 'mounikareddymaddhula',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
